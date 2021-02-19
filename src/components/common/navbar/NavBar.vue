aec2611f16aa73cbcb475eda6b095e3015e7f104
GoodMan-jiaan <2395638834@qq,com>
2021-02-19 22:42:36 +0800

    '首部导航栏的封装，axios的封装，首页网络请求'

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="nav-bar">
+      <div class="left"><slot name="left"></slot></div>
+      <div class="center"><slot name="center" ></slot></div>
+      <div class="right"><slot name="right"></slot></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "NavBar"
+    }
+</script>
+
+<style>
+  .nav-bar{
+    display: flex;
+    height: 44px;
+    text-align: center;
+    /*line-height文字可以上下活动,需要内容进行撑开*/
+    line-height: 44px;
+    /*给tab-bar的上面添加阴影*/
+    box-shadow: 0 1px 1px rgba(100,100,100,.2);
+  }
+
+  .left,.right{
+    width: 60px;
+
+  }
+
+  .center{
+    /*当其他部分有确切的值时，flex等于1能使其他部分填充*/
+    /*flex:1表达的是比例，因为这里只有一个比例，所以他就占据了剩下所有空间的百分之百*/
+    flex: 1;
+
+  }
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,44 @@
 <template>
-  <h2>主页</h2>
+  <div id="home">
+    <NavBar class="home-nav">
+      <template #center>
+        <div>首页</div>
+      </template>
+    </NavBar>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Home"
+  import NavBar from "components/common/navbar/NavBar";
+  import {getHomeMultidata} from "network/home";
+
+  export default {
+    name: "Home",
+    components:{
+      NavBar
+    },
+    data(){
+      return{
+        banners:[],
+        recommends:[]
+      }
+    },
+    //生命周期函数，刚开始创建改组件时
+    created() {
+     //1、请求多个数据
+     getHomeMultidata().then(res=>{
+       console.log(res);
+       this.banners=res.data.data.banner.list
+       this.recommends=res.data.data.recommend.list
+     })
     }
+  }
 </script>
 
 <style scoped>
-
+  .home-nav{
+    /*使用之前定义的变量*/
+    background-color: var(--color-tint);
+    color: #ffffff;
+  }
 </style>
