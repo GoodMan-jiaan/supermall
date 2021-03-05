3ac6227560f4b7817c015747c0ec0ac83f9e40cc
GoodMan-jiaan <2395638834@qq.com>
2021-03-05 17:31:25 +0800

    '使用fastclick插件解决移动端300ms的延迟'

--- src/main.js
+++ src/main.js
@@ -7,5 +7,9 @@ import router from "./router";
 
 import store from "./store";
 // import toast from "components/common/toast".use(toast)
+//解决移动端三百毫秒的延迟
+import FastClick from 'fastclick'
+FastClick.attach(document.body)
+
 
 createApp(App).use(router).use(store).mount('#app')
